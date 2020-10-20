17071e5e20fb3c6de493b97d8e5f4f685c164492
IlyaVoronOFF <iluha.voronov@yandex.ru>
2020-10-20 20:52:15 +0300

    plugins

--- 3-STUDENTS/Воронов Илья/Project/src/index.js
+++ 3-STUDENTS/Воронов Илья/Project/src/index.js
@@ -1,2 +1,5 @@
-import App from "./components";
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './layout/css/style.css';
+import App from './components';
 App();
