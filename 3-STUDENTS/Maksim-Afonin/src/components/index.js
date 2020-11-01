412f1e7ef0acedccbcd5bd7cb4b0d09b773f53e5
[towelee] <nikak.5@mail.ru>
2020-11-01 20:06:09 +1000

    сommit999

--- 3-STUDENTS/Maksim-Afonin/src/components/index.js
+++ 3-STUDENTS/Maksim-Afonin/src/components/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Basket from './basket.js'
 import Catalog from './catalog.js'
 
@@ -6,12 +5,3 @@ export default () => {
     let bascet = new Basket();
     let catalog = new Catalog(bascet);
 }
-=======
-import Basket from './basket.js'
-import Catalog from './catalog.js'
-
-export default () => {
-    let bascet = new Basket();
-    let catalog = new Catalog(bascet);
-}
->>>>>>> 8200e155c6dfe350abda3703e40087d2adce599f
--- 3-STUDENTS/Maksim-Afonin/src/index.js
+++ 3-STUDENTS/Maksim-Afonin/src/index.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import './layout/css/style.css'
 import App from './components'
 
-=======
-import './layout/css/style.css'
-import App from './components'
-
->>>>>>> 8200e155c6dfe350abda3703e40087d2adce599f
 App();
